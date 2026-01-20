1d78b57ba9b3d25338e3a05d650a90004edc10c1
Ramis <ramis5564629@gmail.com>
2026-01-20 19:19:01 +0300

    ADD tests for ProfileRestController

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,70 @@
 package com.javarush.jira.profile.internal.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javarush.jira.BaseIntegrationTest;
+import com.javarush.jira.common.BaseHandler;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static com.javarush.jira.login.internal.web.UserTestData.USER_MAIL;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class ProfileRestControllerTest extends BaseIntegrationTest {
+    private static final String REST_URL_PROFILE = BaseHandler.REST_URL + "/profile";
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void getUnauthorized() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL_PROFILE))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL_PROFILE))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void update() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL_PROFILE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(ProfileTestData.getNewTo())))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateInvalidRequestBody() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL_PROFILE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(ProfileTestData.getInvalidTo())))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    void updateUnauthorized() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL_PROFILE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{}"))
+                .andExpect(status().isUnauthorized());
+
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateUnsupportedMediaType() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL_PROFILE))
+                .andExpect(status().isUnsupportedMediaType());
+    }
 
 }
